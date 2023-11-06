082de232fddda8c96110aef8b39ea23a52fabd91
Harinath-coder <147058948+Harinath-coder@users.noreply.github.com>
2023-11-06 10:43:10 +0530

    Delete employee directory

--- employee/src/main/java/com/example/employee/CustumExceptions/InvalidEmpId.java
+++
@@ -1,7 +0,0 @@
-package com.example.employee.CustumExceptions;
-
-public class InvalidEmpId extends Exception{
-    public InvalidEmpId(String message){
-        super(message);
-    }
-}
--- employee/src/main/java/com/example/employee/Repositories/UserCredentialRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.employee.Repositories;
-
-import com.example.employee.Models.UserCredential;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserCredentialRepo extends JpaRepository<UserCredential,Integer> {
-}
